9697981eb54d4c44dfe50143c49ec208b78280dc
emeric boyadjian <emericboy73@gmail.com>
2022-03-25 18:05:07 +0100

    add SimpleFull to Home and it's contents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Double from "../components/Double";
 import Landing from "../components/Landing";
+import SimpleFull from "../components/SimpleFull";
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Double flexDirection="row" imageName="rainbow.png" title="Rainbow Ice" paragraphOne="Découvrez notre glace multicolore aux multiples parfums. Retrouvez les goûts de la framboise, banane, fraise ainsi que des bonbons “schtroumpf”. Ce mélange de saveurs vous procurera un délicieux gout de friandise." paragraphTwo="Sans ajout de colorant, arôme ou conservateur, notre glace est directement fabriquée dans nos ateliers." />
       <Double flexDirection="row-reverse" imageName="gold.png" title="Ice Gold" paragraphOne="Découvrez notre glace intensément noire au sésame noir parsemée de feuille d'or. Cette crème glacée est réalisée avec une pure pâte de sésame noir du Japon. Ces graines au gout fumé apportent une légère tonalité de bois de réglisse." paragraphTwo="Sans colorant, arôme ou conservateur, notre glace est directement fabriquée dans nos ateliers." />
       <Double flexDirection="row" imageName="blueBanana.png" title="Banane Bleue" paragraphOne="Découvrez notre glace artisanale à la banane bleu ayant un goût similaire à la glace à la vanille. Cette crème glacé est issue de banane bleu d’origine française car contrairement à la banane jaune celle-ci résiste à un climat froid." paragraphTwo="Sans colorant, arôme ou conservateur, notre glace est directement fabriquée dans nos ateliers par nos soins." />
+      <SimpleFull imageName="mystery.jpg" title="Glace mystère" paragraph="Découvrez dès maintenant notre glace au parfum mystère. Avec un colorant noir naturel, sans arômes ou conservateur notre crème glacée est directement fabriquée dans nos ateliers par nos soins. Avec notre glace mystère vous avez la possibilité de tomber sur n'importe quel goût !" />
     </div>
   )
 }
