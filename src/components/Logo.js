644b79cc60ac5306b9a8644b0ec44257e6571a44
emeric boyadjian <emericboy73@gmail.com>
2022-03-26 15:44:05 +0100

    Update Logo : add link to home

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Logo = () => {
   return (
     <div className='logo'>
-      <img src="/img/logo.png" alt="Ice Palace Logo" />
+      <a href="/">
+        <img src="/img/logo.png" alt="Ice Palace Logo" />
+      </a>
     </div>
   );
 };
