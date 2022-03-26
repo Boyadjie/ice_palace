de48c74bad9fd87e8a39b95ddfa4bb900552e2f5
emeric boyadjian <emericboy73@gmail.com>
2022-03-26 15:04:41 +0100

    Add header on new pages

--- src/pages/Rainbow.js
+++ src/pages/Rainbow.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Navigation from '../components/Navigation';
+import Header from '../components/Header';
 
 const Rainbow = () => {
   return (
     <div>
-      <Navigation />
+      <Header />
       
     </div>
   );
