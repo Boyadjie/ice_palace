fea87afd1aa6ce426d07126e6f86afb74d741280
emeric boyadjian <emericboy73@gmail.com>
2022-03-27 16:14:19 +0200

    Completed Landing : style and responsive. Status rate : 100%

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const Landing = (props) => {
+  const scrollTo = () => {
+    window.scrollTo(0, 800);
+  }
+
   return (
     <div className='landingPage'>
       <div className='landingImg'>
         <img src={`/img/home/${props.imageName}`} alt="Landing page" />
-        <a className='btn viewMore' href={props.link}>VOIR PLUS</a>
+        <a className='btn' href={props.link}>VOIR PLUS</a>
       </div>
       <div className='scrollBtn'>
-        <p><img src="/img/home/scroll.svg" alt="Down arrow" /></p>
+        <p onClick={scrollTo}><img src="/img/home/scroll.svg" alt="Down arrow" /></p>
       </div>
     </div>
   );
