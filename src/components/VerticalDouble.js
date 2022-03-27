235272eddb0e00890da8c96afa466c56b59701e4
emeric boyadjian <emericboy73@gmail.com>
2022-03-27 18:05:41 +0200

    add favicon + created component VerticalDouble + add Footer to iceCream page

---
+++ src/components/VerticalDouble.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VerticalDouble = () => {
+  return (
+    <div className='verticalDouble'>
+      
+    </div>
+  );
+};
+
+export default VerticalDouble;
--- src/pages/IceCream.js
+++ src/pages/IceCream.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const IceCream = () => {
   return (
     <div className='ice-cream'>
       <Header />
-      
+      <Footer />
     </div>
   );
 };
