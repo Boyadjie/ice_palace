f5f5af5d72b2200c08d81a95ae0f67517a664f53
SoloGames7 <moreau.solo11@gmail.com>
2022-03-30 11:48:11 +0200

    about & contact pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from "../components/Header";
+import Footer from '../components/Footer';
+
 
 const About = () => {
   return (
     <div className='about'>
       <Header />
       
+      <div className='containerAbout'>
+        <h1>Qui sommes nous ?</h1>
+        <p className='paraAbout'>Ice Palace est une agence de vente en ligne de glaces, proposant des produits originaux élégants et attirants.</p>
+        <div className='contenu'>
+          <div className='CTop'>
+            <img className='imgAbout' src="img/rainbow/about.jpg" alt="" />
+            <p className="rightPara">Bienvenue sur notre site de ventes de glaces en ligne, Ice palace !<br/>
+Toutes nos glaces sont réalisées artisanalement. Nous proposons une large gamme de saveurs de glaces classiques mais nous proposons aussi une sélection de glace que vous ne verrez nul pars ailleurs. Ice palace s’engage à utiliser les meilleurs produits pour la composition de ses glaces. Laissez-vous tenter par notre savoir-faire et la qualité de nos crèmes glacées.
+Et si vous ne parvenez pas à choisir parmi toutes nos crèmes glacées vous avez la possibilité de laisser le hasard décider pour vous grâce à nos glaces mystères composées d’un colorant noir naturel.</p>
+          </div>
+          <button className='btn'>Commander maintenant</button> 
+        </div>
+      </div>
+
+
+      <Footer />
     </div>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Header from "../components/Header";
+import Footer from '../components/Footer';
 
 const Contact = () => {
   return (
     <div className='contact'>
       <Header />
-      
+
+
+      <div className='containerContact'>
+        <h1>NOUS CONTACTER</h1>
+        <p className='paraContact'>Si vous avez des saveurs que vous aimeriez voir sur notre site à nous proposer, n'hésitez pas à nous contacter via ce formulaire. Solicitez nous pour tout autre suggestion ou retour sur nos glaces !</p>
+
+        <form className='form'>
+          <div className='leftForm'>
+            <input className='input' placeholder="Nom*" type="text" name="name" />
+            <input className='input'placeholder="Prénom*"type="text" name="firstname" />            
+            <input className='input' type="email" placeholder='Adresse mail*' name="email" />
+            <input className='input'type="text"name="sujet" placeholder='Sujet du message*'/>
+            <textarea className='input' placeholder='Message*'></textarea>
+            <input className='btn' type="submit" value="Envoyer" />
+          </div>
+
+          <div className='rightForm'>
+            <img className='map' src="img/rainbow/map.svg" alt="CARTE DE LA FRANCE" />
+            <p className='formAdress'>Ice Palace Lyon<br/>6 place Saint Nizier<div/>69002 Lyon<br/><br/>Telephone<br/>07 61 70 23 43</p>
+          </div>
+        </form>
+      </div>
+
+
+      <Footer />
     </div>
   );
 };
